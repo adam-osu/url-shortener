ce26336081126d3ad90f74257799a699fbfb5f5d
adam-osu <okashaa@oregonstate.edu>
2021-08-07 00:38:46 -0400

    Use req.headers.host to send back url

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/shorten", (req, res) => {
       console.error(error);
       return res.status(500).send({ message: "Internal server error" });
     }
-    res.status(200).send({ shortenedUrl: `http://localhost:4000/${shortId}` });
+    res.status(200).send({ shortenedUrl: `${req.headers.host}/${shortId}` });
   });
 });
 
